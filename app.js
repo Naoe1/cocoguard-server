a61a7c43bbdb76908810e6f7dc30150ef2313307
Noea <gllnaoe@gmail.com>
2025-06-16 17:35:08 +0800

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+app.get("/", (req, res) => {
+  res.send("CocoGuard API is running");
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+  console.error(err.stack);
+
+  const response = {
+    message: err.message || "An unknown error occurred",
+  };
+  if (err.validationErrors) {
+    response.errors = err.validationErrors;
+  }
+
+  res.status(err.statusCode || err.code || 500).json(response);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
